20b2b28bba074f436839367bfa27b5a99b84a74c
RyuYii <ruddysurco30@gmail.com>
2021-01-10 12:02:42 -0400

    Agregando la pagina desde startbootstrap, y modificacion de archivos para que todo cuadre

---
+++ public/javascripts/map.js
@@ -0,0 +1,9 @@
+
+var c = [-16.54655,-68.02214]
+var mymap = L.map('mapid').setView(c, 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+L.marker(c).addTo(mymap)
+    .bindPopup('Nos Ubicamos aqui mero!!!<br> Easily customizable.')
+    .openPopup();
